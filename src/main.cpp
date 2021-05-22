8c3f2265229efa2ad10ef2fa9544479edc38513d
Zoë Sparks <zoe@milky.flowers>
2021-05-22 03:34:55 -0400

    Add Vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,13 @@
  * You should have received a copy of the GNU General Public
  * License along with Crypt Underworld. If not, see
  * <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2021 Zoë Sparks <zoe@milky.flowers>
  */
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
 
 int main(void)
 {
@@ -25,6 +29,26 @@ int main(void)
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
     }
+
+    if (SDL_Vulkan_LoadLibrary(NULL) != 0) {
+        SDL_Log("Unable to load Vulkan lib: %s", SDL_GetError());
+        return 1;
+    }
+
+    VkInstance vk_inst;
+    VkInstanceCreateInfo vk_inst_inf = {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = NULL,
+        .flags = 0,
+        .pApplicationInfo = NULL,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = NULL,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = NULL
+    };
+    vkCreateInstance(&vk_inst_inf, NULL, &vk_inst);
+
+    vkDestroyInstance(vk_inst, NULL);
 
     SDL_Quit();
 
