fecfbe1e269aa344b64464ed03b1d2112f6abccf
Zoë Sparks <zoe@milky.flowers>
2021-05-31 17:57:55 -0400

    Document SDL

--- include/sdl.hpp
+++ include/sdl.hpp
@@ -34,19 +34,59 @@ namespace cu {
 
 class Instance;
 
+/*!
+ * \brief The size of the drawable area of the platform window
+ * spawned by SDL.
+ */
 struct WinSize {
     int width;
     int height;
 };
 
+/*!
+ * \brief An SDL wrapper class.
+ */
 class SDL {
 public:
+    /*!
+     * \brief A convenient way to check whether or not an SDL
+     * operation succeeded. Logs the attempt if logging is turned
+     * on. Throws if SDL reports an error.
+     *
+     * \param result The result returned by the SDL operation.
+     *
+     * \param oper A description of what is being attempted (see
+     * Log::attempt()).
+     */
     static void sdl_try(int result, std::string oper);
+    /*!
+     * \overload
+     */
     static void sdl_try(void* result, std::string oper);
+    /*!
+     * \overload
+     */
     static void sdl_try(SDL_bool result, std::string oper);
+
+    /*!
+     * \brief Throws an error, reporting the error message
+     * returned by SDL.
+     *
+     * \param oper A description of what was being attempted.
+     */
     static void sdl_throw(std::string oper);
+
+    /*!
+     * \brief Returns a pointer to a function that can be used to
+     * load Vulkan functions (see
+     * [`SDL_Vulkan_GetVkGetInstanceProcAddr()`](https://wiki.libsdl.org/SDL_Vulkan_GetVkInstanceProcAddr)).
+     */
     static PFN_vkGetInstanceProcAddr get_get_inst_proc_addr();
 
+    /*!
+     * \brief (constructor) Initializes SDL, including the
+     * spawning of a platform window.
+     */
     SDL();
 
     SDL(SDL&&) = delete;
@@ -55,10 +95,30 @@ public:
 
     ~SDL() noexcept;
 
+    /*!
+     * \brief Returns the names of the Vulkan extensions required
+     * by SDL.
+     */
     std::vector<const char*> get_req_vulk_exts() const;
 
-    void create_surface(Instance&, VkSurfaceKHR*);
+    /*!
+     * \brief Creates a Vulkan rendering surface for the platform
+     * window. Unless you have a specific reason you should
+     * create a Surface instead of calling this, which will
+     * handle this process automatically.
+     *
+     * \param inst The Vulkan instance in use.
+     *
+     * \param surf A pointer to the Vulkan surface handle to
+     * initialize. Doesn't take a Surface& because this function
+     * is needed in Surface initialization.
+     */
+    void create_surface(Instance& inst, VkSurfaceKHR* surf);
 
+    /*!
+     * \brief Returns the size of the drawable area of the
+     * platform window.
+     */
     WinSize get_win_size();
 
 private:
