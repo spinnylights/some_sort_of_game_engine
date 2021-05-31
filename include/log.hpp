64f3bb46fa5f6ee2c2ebf8bb3d2bf370655d97a6
Zoë Sparks <zoe@milky.flowers>
2021-05-31 16:35:24 -0400

    Document logging classes

--- include/log.hpp
+++ include/log.hpp
@@ -32,19 +32,28 @@
 
 namespace cu {
 
-// see /The C++ Programming Language/, 4th ed., pg. 1215
-//
-// this should probably be changed to use std::jthread once e.g.
-// debian stable switches to gcc >= 10
+/*!
+ * \brief A wrapper class around a std::thread that joins the
+ * thread automatically on destruction.
+ *
+ * (based on a class in _The C++ Programming Language_, Bjarne
+ * Stroustrup, 4th ed., pg. 1215)
+ */
 struct GuardedThread {
     std::thread t;
 
+    /*!
+     * \brief (move assignment operator)
+     */
     GuardedThread& operator=(GuardedThread&& other)
     {
         std::swap(t, other.t);
         return *this;
     }
 
+    /*!
+     * \brief Joins the thread, if it is joinable.
+     */
     void join()
     {
         if (t.joinable()) {
@@ -52,31 +61,85 @@ struct GuardedThread {
         }
     }
 
+    /*!
+     * \brief (destructor)
+     *
+     * \copydoc join()
+     */
     ~GuardedThread()
     {
         join();
     }
+private:
+    // this class should probably be substituted for std::jthread
+    // once e.g. debian stable switches to gcc >= 10
 };
 
+/*!
+ * \brief A single entry in the LoggableObj output format.
+ *
+ * Corresponds to one "member" of the "loggable object". As a
+ * general rule, this class is not intended to be instantiated
+ * directly, but rather to be used in the composition of
+ * LoggableObj.
+ */
 struct LoggableObjMember {
+    /*!
+     * \brief The string to insert between the name and the
+     * value.
+     */
     static const inline std::string spacer = ": ";
 
+    /*!
+     * \brief The name of the member.
+     */
     std::string name;
+    /*!
+     * \brief The value of the member.
+     */
     std::string value;
 
+    /*!
+     * \brief The whole string to output.
+     *
+     * \param opening_chars The desired number of characters for
+     * the "opening" (name + spacer) to take up. Uses spaces
+     * between the spacer and name to make up the difference if
+     * needed. See Log for an example.
+     *
+     * \param pre_spaces The number of spaces to insert before
+     * the "opening" (i.e. for indentation).
+     */
     std::string str(std::string::size_type opening_chars = 0,
                     std::string::size_type pre_spaces = 0);
 
+    /*!
+     * \brief (constructor) A single name-value pair.
+     *
+     * \param nme \copydoc name
+     * \param vle \copydoc value
+     */
     LoggableObjMember(std::string nme, std::string vle)
         :name{nme},
          value{vle}
     {}
 
+    /*!
+     * \brief (constructor) A single name-value pair, C-string
+     * version.
+     *
+     * \param nme \copydoc name
+     * \param vle \copydoc value
+     */
     LoggableObjMember(std::string nme, const char* vle)
         :name{nme},
          value{vle}
     {}
 
+    /*!
+     * \brief (constructor) A name and a list of associated
+     * values.
+     */
     LoggableObjMember(std::string nme, std::vector<std::string> const& values)
         :name{nme}
     {
@@ -88,23 +151,119 @@ struct LoggableObjMember {
         value += values.back();
     }
 
+    /*!
+     * \brief (constructor) "Duck-typed" value logging over
+     * `std::to_string()`.
+     */
     template <typename T> LoggableObjMember(std::string nme, T vle)
         :name{nme},
          value{std::to_string(vle)}
     {}
 };
 
+/*!
+ * \brief A way to log an "object", i.e. a name and a list of
+ * "members". See Log for an example of the output format.
+ */
 struct LoggableObj {
+    /*!
+     * \brief The name of the object.
+     */
     std::string name;
+    /*!
+     * \brief A list of the object's members, which can assume a
+     * variety of formats (see LoggableObjMember for more).
+     */
     std::vector<LoggableObjMember> members;
 
+    /*!
+     * \brief The formatted string.
+     */
     std::string str();
 };
 
+/*!
+ * \brief A logging class. Has a variety of output formats,
+ * easily-controlled indentation and line breaks, and can be
+ * switched between synchronous and asynchronous operation at
+ * runtime.
+ *
+ * For the time being, this class is not designed to be copied or
+ * moved, as with some of the game engine classes. An instance
+ * called `log` is available globally by including the header
+ * `log.hpp`.
+ *
+ * It should be safe to call the various logging functions from
+ * within destructors. The one thing you should not do from a
+ * destructor is sync/async switching.
+ *
+ * This class's functions are not designed to be called from
+ * multiple threads. This will be changed in the future if
+ * necessary.
+ *
+ * For the time being, it just writes to stdout (unless it enters
+ * an error state internally). This may be changed in the future
+ * if it seems desirable.
+ *
+ * Some examples of use:
+ *
+ * ```
+ * log.enter("meowing begins");
+ *
+ * // meowing begins
+ *
+ * log.attempt("Meower", "meowing");
+ *
+ * uint32_t meow_cnt = 3;
+ * meower.meow(meow_cnt);
+ *
+ * log.finish();
+ * log.indent();
+ * log.enter("times meowed", meow_cnt);
+ * log.indent(2);
+ * log.enter("meow sounds", { "mrrr", "mrrrau", "maaaaau" });
+ * log.brk();
+ * log.enter("line breaks reset indentation");
+ *
+ * // Meower: meowing...OK
+ * //     times meowed: 3
+ * //         meow sounds: mrrr, mrrrau, maaaaau
+ * //
+ * // line breaks reset indentation
+ *
+ * log.enter({
+ *     .name = "Meowers",
+ *     .members = {
+ *         { "colors",              "many" },
+ *         { "legs",                4      },
+ *         { "body temp",           101.5 },
+ *         { "some i have known", { "Big Boy",
+ *                                  "Lyla",
+ *                                  "Fly",
+ *                                  "Maxine",
+ *                                  "Fifi",
+ *                                  "Cerise" }}
+ *     }
+ * });
+ *
+ * // Meowers { colors:            many,
+ * //           legs:              4,
+ * //           body temp:         101.500000,
+ * //           some i have known: Big Boy
+ * //                              Lyla
+ * //                              Fly
+ * //                              Maxine
+ * //                              Fifi
+ * //                              Cerise }
+ * ```
+ */
 class Log {
 public:
     static const inline std::string indentation = "    ";
 
+    /*!
+     * \brief (constructor)
+     */
     Log();
 
     Log(Log&&) = delete;
@@ -113,24 +272,120 @@ public:
 
     ~Log() noexcept;
 
+    /*!
+     * \brief Write a string.
+     *
+     * \param entry The string to write.
+     *
+     * \param newline Whether to write a newline after the entry
+     * or not.
+     */
     void enter(std::string entry, bool newline=true) noexcept;
+
+    /*!
+     * \brief Write a name, a separator, and a list of entries.
+     *
+     * \param name The name to write, followed by a separator.
+     *
+     * \param entries A list of entries. Written comma-separated.
+     */
     void enter(std::string name,
                std::vector<const char*> const& entries) noexcept;
+
+    /*!
+     * \brief "Duck-typed" name-entry logging.
+     *
+     * \param obj The object name to write.
+     *
+     * \param attr The value of the object. Can be anything
+     * supported by std::to_string().
+     */
     template <typename T> void enter(std::string obj, T attr) noexcept
     {
         enter(obj, std::to_string(attr));
     }
+
+    /*!
+     * \brief Name-entry logging where both are strings.
+     *
+     * \param obj The object name to write.
+     *
+     * \param attr The value to write.
+     */
     void enter(std::string obj, std::string attr) noexcept;
+
+    /*!
+     * \brief A convenient way to display a complex object. See
+     * Log for an example.
+     *
+     * \param obj The object to display.
+     */
     void enter(LoggableObj&& obj) noexcept;
+
+    /*!
+     * \brief Prints the entry followed by an ellipse and no
+     * newline. Designed to be used in concert with finish() or
+     * the like for operations that may fail.
+     *
+     * \param entry A description of what is being attempted.
+     */
     void attempt(std::string entry) noexcept;
+
+    /*!
+     * \brief Like attempt(std::string), but using the
+     * name-entry format.
+     *
+     * \param domain The domain or category the operation falls under.
+     *
+     * \copydetails attempt(std::string)
+     */
     void attempt(std::string domain, std::string entry) noexcept;
-    void indent() noexcept;
-    void indent(std::string::size_type amt) noexcept;
+
+    /*!
+     * \brief Prints "OK" and a newline. Designed to be used with
+     * attempt(); you can sandwich an operation that may fail
+     * between them.
+     */
     void finish() noexcept;
+
+    /*!
+     * \brief Turns on indentation. Everything after this will be
+     * indented until indent(0) or brk() is called.
+     */
+    void indent() noexcept;
+
+    /*!
+     * \brief Like indent(), but allows the degree of indentation
+     * to be set.
+     *
+     * \param amt How many indentations to precede the string
+     * with.
+     */
+    void indent(std::string::size_type amt) noexcept;
+
+    /*!
+     * \brief Inserts a line break and turns off indentation.
+     */
     void brk() noexcept;
+
+    /*!
+     * \brief Enables logging.
+     */
     void turn_on() noexcept;
+
+    /*!
+     * \brief Disables logging.
+     */
     void turn_off() noexcept;
+
+    /*!
+     * \brief Starts asynchronous logging (off by default).
+     */
     void async_on();
+
+    /*!
+     * \brief Stops asynchronous logging.
+     */
     void async_off();
 
 private:
